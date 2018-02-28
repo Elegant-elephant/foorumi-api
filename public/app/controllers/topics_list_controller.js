8bd326b2174e9c85422cbd397aacb92f95f90ec9
Xpdmk <roy.jarvinen@gmail.com>
2018-02-28 11:12:55 +0200

    Implemented real topic list return

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,8 +1,6 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  // Toteuta kontrolleri tähän
-  /* Api.getTopics().success(function(topics) {
+  Api.getTopics().success(function(topics) {
     $scope.topics = topics
-  }) */
-  $scope.topics = [{name: "Name", description: "Desc"}]
+  })
   $scope.addTopic = () => Api.addTopic({name: $scope.newTopic.name, description: $scope.newTopic.description})
 });
