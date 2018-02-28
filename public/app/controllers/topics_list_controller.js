c755e07d59ff7b49ed9788e0b4da31063a9b4eba
Xpdmk <roy.jarvinen@gmail.com>
2018-02-28 11:01:22 +0200

    Implemented topics list controller placeholder

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,3 +1,8 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api){
   // Toteuta kontrolleri tähän
+  /* Api.getTopics().success(function(topics) {
+    $scope.topics = topics
+  }) */
+  $scope.topics = [{name: "Name", description: "Desc"}]
+  $scope.addTopic = () => Api.addTopic({name: $scope.newTopic.name, description: $scope.newTopic.description})
 });
