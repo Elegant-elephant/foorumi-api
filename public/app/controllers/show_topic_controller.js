b257ed174d3e87af9f6751e7dedb2e25a8902ca8
lauri <lauri@xip.fi>
2018-02-28 13:59:29 +0200

    added create message and list messages functionality to topic page

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,3 +1,20 @@
 FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api){
   // Toteuta kontrolleri tähän
+  Api.getTopic($routeParams.id).then( res => {
+    $scope.topic = res.data;
+  })
+
+  $scope.formatDate = function(date) {
+    date = new Date(date);
+    const options = {  
+      weekday: "long", year: "numeric", month: "numeric",  
+      day: "numeric", hour: "2-digit", minute: "2-digit"  
+    };  
+    return date.toLocaleTimeString("fi-FI", options);
+  }
+
+  $scope.addMessage = () => {
+    console.log($scope.newMessage)
+    Api.addMessage({title: $scope.newMessage.title, content: $scope.newMessage.content}, $routeParams.id);
+  }
 });
