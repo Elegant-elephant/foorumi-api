9029dca9387906bf0516962316aa54dfbebfc773
Xpdmk <roy.jarvinen@gmail.com>
2018-02-28 15:07:07 +0200

    Implemented live reload after submitting topic

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,6 +1,34 @@
-FoorumApp.controller('TopicsListController', function($scope, $location, Api){
-  Api.getTopics().success(function(topics) {
-    $scope.topics = topics
-  })
-  $scope.addTopic = () => Api.addTopic({name: $scope.newTopic.name, description: $scope.newTopic.description})
+FoorumApp.controller('TopicsListController', function($scope, $location, Api) {
+
+  const data = {
+    topics: []
+  }
+
+  const getTopics = () => {
+    Api.getTopics().success((newTopics) => {
+      console.info("getTopics()")
+      console.log($scope)
+      console.log((data.topics.length > 0? 
+          `Old: ${JSON.stringify($scope.topics)}\n`
+          + "New: \n" : "Data: \n"
+        ) + JSON.stringify(newTopics))
+      data.topics = newTopics
+    }) 
+  }
+
+  $scope.addTopic = () => {
+    console.info("addTopic()")
+    console.log($scope)
+    const obj = {name: $scope.newTopic.name, description: $scope.newTopic.description}
+    Api.addTopic(obj)
+      .then(() => {getTopics()})
+      .then(() => console.log($scope))
+  }
+
+  $scope.init = () => {
+    console.info("init()")
+    console.log($scope)
+    $scope.data = data
+    getTopics()
+  }
 });
