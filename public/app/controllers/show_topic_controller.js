bbb6ea8945f8f637fa9a152924f0ad962f02e721
lauri <lauri@xip.fi>
2018-02-28 15:08:45 +0200

    refactored show_topic_controller and implemented live update for new messages.

--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -1,11 +1,15 @@
 FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $location, Api){
   // Toteuta kontrolleri tähän
-  Api.getTopic($routeParams.id).then( res => {
-    $scope.topic = res.data;
-  })
+  function updateTopic(){
+    Api.getTopic($routeParams.id).then( res => {
+      $scope.topic = res.data;
+    })
+  }
+  
+  $scope.init = updateTopic();
 
   $scope.addMessage = () => {
-    console.log($scope.newMessage)
-    Api.addMessage({title: $scope.newMessage.title, content: $scope.newMessage.content}, $routeParams.id);
+    Api.addMessage({title: $scope.newMessage.title, content: $scope.newMessage.content}, $routeParams.id)
+    .then(updateTopic());
   }
 });
