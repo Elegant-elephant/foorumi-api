d3b3ece687891b0902c0fbab919ef375549fe064
Lauri Koskinen <lauri.koskinen2@metropolia.fi>
2018-02-28 16:49:30 +0200

    Refactored code, removed console logs. fixed href.

--- public/app/controllers/topics_list_controller.js
+++ public/app/controllers/topics_list_controller.js
@@ -1,34 +1,17 @@
 FoorumApp.controller('TopicsListController', function($scope, $location, Api) {
 
-  const data = {
-    topics: []
-  }
-
-  const getTopics = () => {
-    Api.getTopics().success((newTopics) => {
-      console.info("getTopics()")
-      console.log($scope)
-      console.log((data.topics.length > 0? 
-          `Old: ${JSON.stringify($scope.topics)}\n`
-          + "New: \n" : "Data: \n"
-        ) + JSON.stringify(newTopics))
-      data.topics = newTopics
+  const updateTopics = () => {
+    Api.getTopics().success((topics) => {
+      $scope.topics = topics
     }) 
   }
 
   $scope.addTopic = () => {
-    console.info("addTopic()")
-    console.log($scope)
-    const obj = {name: $scope.newTopic.name, description: $scope.newTopic.description}
+    const obj = { name: $scope.newTopic.name, description: $scope.newTopic.description }
     Api.addTopic(obj)
-      .then(() => {getTopics()})
-      .then(() => console.log($scope))
+      .then(updateTopics())
   }
 
-  $scope.init = () => {
-    console.info("init()")
-    console.log($scope)
-    $scope.data = data
-    getTopics()
-  }
+  $scope.init = updateTopics();
+
 });
