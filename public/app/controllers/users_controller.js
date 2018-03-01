1e9c7d0af777bc8576e22d4f15f7162cdd118709
Lauri Koskinen <lauri.koskinen2@metropolia.fi>
2018-03-01 20:06:28 +0200

    added redirects when user logs in or registers

--- public/app/controllers/users_controller.js
+++ public/app/controllers/users_controller.js
@@ -1,16 +1,26 @@
 FoorumApp.controller('UsersController', function($scope, $location, Api){
   // Toteuta kontrolleri tähän
+  console.log($scope.user);
   $scope.register = function(){
+
     Api.register({
       username: $scope.newUser.username,
       password: $scope.newUser.password
-    })
+    }).then(function(response){
+      console.log('Rekisteröinti onnistui!');
+      $location.path('/login');
+    });
   }
 
   $scope.login = function(){
     Api.login({
       username: $scope.user.username,
       password: $scope.user.password
-    })
+    }).success(function(user){
+      console.log('Kirjautuminen onnistui!');
+      $location.path('/')
+    }).error(function(){
+      $scope.errorMessage = 'Väärä käyttäjätunnus tai salasana!';
+    });
   }
 });
