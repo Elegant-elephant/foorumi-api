e013346cc3bc9e849cfacae07c440ec0924f3531
lauri <lauri@xip.fi>
2018-03-01 17:18:07 +0200

    implemented register and login.

--- public/app/controllers/users_controller.js
+++ public/app/controllers/users_controller.js
@@ -1,3 +1,16 @@
 FoorumApp.controller('UsersController', function($scope, $location, Api){
   // Toteuta kontrolleri tähän
+  $scope.register = function(){
+    Api.register({
+      username: $scope.newUser.username,
+      password: $scope.newUser.password
+    })
+  }
+
+  $scope.login = function(){
+    Api.login({
+      username: $scope.user.username,
+      password: $scope.user.password
+    })
+  }
 });
