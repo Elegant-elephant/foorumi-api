64b2d3e2905c0b11315b8c20ad5dec6eb88bcda2
Lauri Koskinen <lauri.koskinen2@metropolia.fi>
2018-03-04 12:21:46 +0200

    Register form tells if username is taken. Changed some ng-show attributes

--- public/app/controllers/users_controller.js
+++ public/app/controllers/users_controller.js
@@ -3,15 +3,24 @@ FoorumApp.controller('UsersController', function($scope, $location, Api){
   $scope.user = {username: null, password: null};
   
   $scope.register = function(form){
+    $scope.errorMessage = null;
+    form.$setUntouched();
+    form.$setPristine();
     if(form.$valid){
-      Api.register($scope.user).then(function(response){
+      Api.register($scope.user).success(function(response){
         console.log('Rekisteröinti onnistui!');
-        $location.path('/login');
+        $location.path('/login')
+      }).error(function(response){
+        console.log('Rekisteröinti epäonnistui!');
+        $scope.errorMessage = response.error;
       });
     }
   }
 
   $scope.login = function(form){
+    $scope.errorMessage = null;
+    form.$setUntouched();
+    form.$setPristine();
     if(form.$valid){
       Api.login($scope.user)
       .success(function(user){
